76cbb36413cbeafcfd0b5f92f0af52d2e1d85e26
Vi1cius <suicnivi@gmail.com>
2025-07-25 19:54:55 -0300

    Update GrupoClientes.java

--- src/main/java/model/GrupoClientes.java
+++ src/main/java/model/GrupoClientes.java
@@ -1,6 +1,6 @@
-package model; 
+package java.model; 
 
-import util.StatusAtendimento;
+import java.util.StatusAtendimento;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
